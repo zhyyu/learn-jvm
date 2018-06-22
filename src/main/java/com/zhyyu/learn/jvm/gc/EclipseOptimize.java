22bd727cc35b10be46f627fcb225f7a5da3da8a8
zhongyu_yu <zhongyu_yu@kingdee.com>
2018-06-22 11:25:55 +0800

    add eclipse optimize

---
+++ src/main/java/com/zhyyu/learn/jvm/gc/EclipseOptimize.java
@@ -0,0 +1,21 @@
+package com.zhyyu.learn.jvm.gc;
+
+/**
+ * optimize eclipse 
+ * 
+-Xms1024m
+-Xmx1024m
+-Xmn512m
+-XX:+UseParNewGC
+-XX:+UseConcMarkSweepGC
+-XX:CMSInitiatingOccupancyFraction=85
+ * @author Administrator
+ *
+ */
+public class EclipseOptimize {
+
+	public static void main(String[] args) {
+		while(true);
+	}
+	
+}

f6e145b972f3b04f5725e7ca21b188f19b29e665
= <zhyyu2016@gmail.com>
2018-07-18 23:47:43 +0800

    auto generate dump when oom, set dump path

--- src/main/java/com/zhyyu/learn/jvm/oomsof/HeapOOM.java
+++ src/main/java/com/zhyyu/learn/jvm/oomsof/HeapOOM.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * -Xms10m -Xmx10m -XX:+HeapDumpOnOutOfMemoryError
+ * -Xms10m -Xmx10m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap_dump
+ * <pre>
+ * -XX:HeapDumpPath 当前目录是执行java 命令的目录
  * @author zhongyuyu
  * 
  */
@@ -24,16 +26,16 @@ public class HeapOOM {
 
 /**
  * java.lang.OutOfMemoryError: Java heap space
-Dumping heap to java_pid5208.hprof ...
-Heap dump file created [14558453 bytes in 0.105 secs]
+Dumping heap to ./heap_dump ...
+Unable to create ./heap_dump: File exists
 Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
 	at java.util.Arrays.copyOf(Arrays.java:3210)
 	at java.util.Arrays.copyOf(Arrays.java:3181)
-	at java.util.ArrayList.grow(ArrayList.java:261)
-	at java.util.ArrayList.ensureExplicitCapacity(ArrayList.java:235)
-	at java.util.ArrayList.ensureCapacityInternal(ArrayList.java:227)
-	at java.util.ArrayList.add(ArrayList.java:458)
-	at com.zhyyu.learn.jvm.HeapOOM.main(HeapOOM.java:19)
+	at java.util.ArrayList.grow(ArrayList.java:265)
+	at java.util.ArrayList.ensureExplicitCapacity(ArrayList.java:239)
+	at java.util.ArrayList.ensureCapacityInternal(ArrayList.java:231)
+	at java.util.ArrayList.add(ArrayList.java:462)
+	at com.zhyyu.learn.jvm.oomsof.HeapOOM.main(HeapOOM.java:21)
  * 
  */
 
